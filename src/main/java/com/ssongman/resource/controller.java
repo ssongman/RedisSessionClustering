2832d0668a903ccc54a2b9bcbc8c75518da09481
ssongman <ssongmantop@gamil.com>
2019-03-11 10:44:15 +0900

    first commit

---
+++ src/main/java/com/ssongman/resource/controller.java
@@ -0,0 +1,35 @@
+package com.ssongman.resource;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/hello")
+public class controller {
+	@GetMapping
+	public String hello(HttpSession session) {
+		UUID uid = (UUID) session.getAttribute("uid");
+		String id = (String)session.getAttribute("id");
+
+		System.out.println("session uid : " + uid);
+		System.out.println("session id : " + id);
+		if (uid==null) {
+			uid = UUID.randomUUID();
+		}
+		session.setAttribute("uid", uid);
+		System.out.println("after session uid : " + uid.toString());
+		return "Hello World get";
+	}
+	
+	@PostMapping("/add")
+	public String helloPost(@RequestBody final String hello) {
+		return "Hello world post";
+	}
+}
